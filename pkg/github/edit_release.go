d8b564ea1e716ba6528047ca039f62dd19a7f718
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-10-04 23:37:48 +0900

    feat: reduce GitHub API call
    Instead of getting the release description and updating it temporarily, merely calling the GitHub API without any parameters only once.
    
    Even if the update release API is called without any parameters, the API call succeeds and the release becomes immutable.

--- pkg/github/edit_release.go
+++ pkg/github/edit_release.go
@@ -6,16 +6,10 @@ import (
 	"github.com/google/go-github/v75/github"
 )
 
-type InputEditRelease struct {
-	ID int64
-}
-
-func (c *Client) EditRelease(ctx context.Context, owner, repo string, id int64, description string) error {
+func (c *Client) EditRelease(ctx context.Context, owner, repo string, id int64) error {
 	// GraphQL API does not support updating a release.
 	// https://docs.github.com/en/graphql/reference/mutations
 	// https://pkg.go.dev/github.com/google/go-github/v75/github#RepositoriesService.EditRelease
-	_, _, err := c.repos.EditRelease(ctx, owner, repo, id, &github.RepositoryRelease{
-		Body: github.Ptr(description),
-	})
+	_, _, err := c.repos.EditRelease(ctx, owner, repo, id, &github.RepositoryRelease{})
 	return err //nolint:wrapcheck
 }
